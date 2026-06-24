bdae318abb577f487b2b06393a1232b9624d2d6f
Ed Hagen <ehagen@gmail.com>
2026-06-24 14:10:54 -0700

    Added ios splash screens

--- docs/sw.js
+++ docs/sw.js
@@ -41,3 +41,19 @@ self.addEventListener("fetch", (event) => {
     }),
   );
 });
+
+// Proactive Cache Cleanup Strategy
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Clearing old cache:", cache);
+            return caches.delete(cache);
+          }
+        }),
+      );
+    }),
+  );
+});
